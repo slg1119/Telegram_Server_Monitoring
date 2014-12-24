4629ed9d656b5e3634c06a75c82cee417208dd96
RokHyung Son <fhrgud1264@gmail.com>
2014-12-25 03:08:17 +0900

    Edit Server Script

--- server.lua
+++ server.lua
@@ -1,4 +1,3 @@
-ping_cmd = "sh /home/Username/server/ping.sh"
 cmd_cmd = "sh /home/Username/server/cmd.sh"
 function split(str)
         local arg_1st=""
@@ -28,21 +27,6 @@ function on_msg_receive (msg)
    if ( msg.text == 'stats' ) then
       local t = os.execute("sh /home/Username/server/stats.sh > /home/Username/server/stats.txt")
       send_text(msg.from.print_name, '/home/Username/server/stats.txt', ok_cb, false)
-      return
-   end
-    if ( msg.text == 'ping' ) then
-      local t = os.execute(ping_cmd .. arg)
-      send_text(msg.from.print_name, '/home/Username/server/ping.txt', ok_cb, false)
-      return
-   end
-    if ( msg.text == 'who' ) then
-      local t = os.execute("sh /home/Username/server/who.sh > /home/Username/server/who.txt")
-      send_text(msg.from.print_name, '/home/Username/server/who.txt', ok_cb, false)
-      return
-   end
-    if ( msg.text == 'process' ) then
-      local t = os.execute("sh /home/Username/server/process.sh > /home/Username/server/process.txt")
-      send_text(msg.from.print_name, '/home/Username/server/process.txt', ok_cb, false)
       return
    end
     if ( msg.text == 'help' ) then
